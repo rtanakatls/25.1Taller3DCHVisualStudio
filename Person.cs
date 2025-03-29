637255e22d3edac2b60f943cbb60815179f282a8
Ricardo Tanaka Terukina <ricardotanaka-externo@tgacompany.com>
2025-03-29 08:11:18 -0500

    Created project

---
+++ Person.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Taller3DCH251
+{
+    internal class Person
+    {
+        private string name;
+        private int age;
+
+        public string Name { get { return name; } }
+        public int Age { get { return age; } }
+
+
+        public Person(string name, int age)
+        {
+            this.name = name;
+            this.age = age;
+        }
+    }
+}

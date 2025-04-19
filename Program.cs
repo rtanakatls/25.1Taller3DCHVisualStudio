94b1ba0542bae0b8d844c203cfdb5328ce2b79e1
Ricardo Tanaka Terukina <ricardotanaka-externo@tgacompany.com>
2025-04-19 07:30:35 -0500

    Added classes for strategy game

--- Program.cs
+++ Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Taller3DCH251.Strategy;
 using Taller3DCH251.Week1;
 using Taller3DCH251.Week2;
 
@@ -12,7 +13,7 @@ namespace Taller3DCH251
     {
         static void Main(string[] args)
         {
-            Menu menu = new MenuWeek2();
+            Menu menu = new StrategyMenu();
             menu.Execute();
         }
     }
---
+++ Strategy/StrategyMenu.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Taller3DCH251.Strategy
+{
+    internal class StrategyMenu : Menu
+    {
+        public override void Execute()
+        {
+            Console.WriteLine("Bienvenido al juego de estrategia");
+        }
+    }
+}

981a1fde85ea4a00fc96e6e2ec1cec00660c9717
Ricardo Tanaka Terukina <ricardotanaka-externo@tgacompany.com>
2025-04-19 07:35:22 -0500

    Created Unit

--- Strategy/Unit.cs
+++ Strategy/Unit.cs
@@ -8,5 +8,27 @@ namespace Taller3DCH251.Strategy
 {
     internal class Unit
     {
+        protected string name;
+        protected int life;
+
+        public Unit(string name, int life) 
+        {
+            this.name = name;
+            this.life = life;
+        }
+
+        private void ChangeLife(int value)
+        {
+            life -= value;
+            if(life < 0)
+            {
+                life = 0;
+            }
+        }
+
+        public void TakeDamage(int damage)
+        {
+            ChangeLife(-damage);
+        }
     }
 }

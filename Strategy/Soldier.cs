d1ac6a327cd7e7c099d0337895a077a8c363fd4c
Ricardo Tanaka Terukina <ricardotanaka-externo@tgacompany.com>
2025-04-19 07:36:38 -0500

    Created soldier

---
+++ Strategy/Soldier.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Taller3DCH251.Strategy
+{
+    internal class Soldier : Unit
+    {
+        protected int damage;
+
+        public Soldier(string name, int life, int damage) : base(name,life)
+        {
+            this.damage = damage;
+        }
+
+        public int GetDamage()
+        {
+            return damage;
+        }
+    }
+}

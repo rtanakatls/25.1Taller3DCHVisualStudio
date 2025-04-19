a98e77eecbc443dcc8838aa7002a31614a62456a
Ricardo Tanaka Terukina <ricardotanaka-externo@tgacompany.com>
2025-04-19 07:47:12 -0500

    Created harvester

---
+++ Strategy/Harvester.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Taller3DCH251.Strategy
+{
+    internal class Harvester : Unit
+    {
+    }
+}
